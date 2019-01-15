71ea0ccb80dbe63348c23c0e3c2525e69a6529c6
UsuarioPC <UsuarioPC@DESKTOP-RBFD7SF>
2019-01-15 12:55:56 +0100

    Con el ejercicio uno empezadop solo funciona para calcular el primer maximo ANIMO!!!

---
+++ src/ejemplosconsola/EjemplosConsola.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosconsola;
+import java.util.Arrays;
+/**
+ *
+ * @author UsuarioPC
+ */
+public class EjemplosConsola {
+    //declaro un array de ints de instancia 
+int[] listaNumeros = {200,31,27,2,5,99};
+//el metodo maximos va a calcular el maximo y 
+//el segundo maximo de una lista de numeros 
+private	int[] maximos (int[] lista){
+    //declaramos un array de dos elementos 
+    //para guardar el maximo y el segundo maximo 
+    int [] listaMaximos = {lista[0],lista[0]};
+    
+    for (int i=0;i< lista.length; i++){
+        if (listaMaximos[0] < lista[i]){
+            listaMaximos[1] = listaMaximos[0];
+            listaMaximos[0] = lista[i];
+        }    
+    }
+    return listaMaximos;
+}
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EjemplosConsola ejercicios =new EjemplosConsola();    
+        System.out.println ("HOLA MUNDO!");
+        System.out.println(ejercicios.listaNumeros[2]);
+        System.out.println( Arrays.toString(ejercicios.maximos(ejercicios.listaNumeros)));
+        
+    }
+    
+}
